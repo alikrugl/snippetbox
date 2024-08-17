4cc8b880036e1a876acd5666a47c40160684524f
Alik Kruhlienia <alikrugl11@gmail.com>
2024-08-17 10:03:33 +0300

    add middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,18 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
-    mux := http.NewServeMux()
+	"github.com/justinas/alice"
+)
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+func (app *application) routes() http.Handler {
+	mux := http.NewServeMux()
 
-    mux.HandleFunc("GET /{$}", app.home)
-    mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-    mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-    mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-    return mux
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	return standard.Then(mux)
 }
-
